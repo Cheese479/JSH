8ad11640120de8171e4b2e54afe41c5b7d3fd5b7
Cheese479 <181613388+Cheese479@users.noreply.github.com>
2025-11-18 19:18:16 -0600

    Update James Shell to version 1.1 with new commands
    Added:
    Made command names more Unix like
    Added 'jsh' which is like cowsay
    Added 'blush' which prints '>_<'!!!
    Added 'calc' which calculates arithmetic
    Added 'cookie' which is a mini cookie clicker game
    Added 'run' for line-by-line .jsh scripting support

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 import random
 import webbrowser
+import time
+import threading
+import shutil
 
 #JSH Setup
-cd = os.path.abspath(__file__)
+cd = os.getcwd()
 running = True
 fcolor_code = "\033[1;37m"
 bcolor_code = "\033[1;40m"
@@ -12,7 +15,7 @@ def prompt():
     """Prompt user for command using current colors and working directory"""
     return input(f"{bcolor_code}{fcolor_code}JSH@{cd}>\033[0m ").strip().split(" ", 1)
 
-print("Welcome to James Shell Version 1.0!")
+print("Welcome to James Shell Version 1.1!")
 print("Type 'help' for commands!")
 print(f"Current Directory: {cd}")
 command = prompt()
@@ -30,10 +33,10 @@ def help():
           "pwd - print working directory\n"
           "cd <dir> - change directory\n"
           "ls <dir> - list directory\n"
-          "newdir <dir> - makes a new directory\n"
+          "mkdir <dir> - makes a new directory\n"
           "rm <file/dir> - deletes a file or folder (caution!)\n"
-          "newfile <file> - makes a new file\n"
-          "printfile <file> - prints the contents of a file\n"
+          "touch <file> - makes a new file\n"
+          "cat <file> - prints the contents of a file\n"
           "edit <file> - edit a file’s content\n"
           "ver - show version\n"
           "clear - clear screen\n"
@@ -41,6 +44,11 @@ def help():
           "random <min>(newline)<max> - random number between min and max\n"
           "fcolor <color> - set foreground text color\n"
           "bcolor <color> - set background color\n"
+          "jsh - print JSH ASCII art\n"
+          "calc <expression> - calculate a mathematical expression\n"
+          "blush - print >_<\n"
+          "cookieclicker - play cookie clicker game\n"
+          "run <.jsh script file> - run a JSH script file\n"
           "quit - close JSH\n")
 
 def changedir(path=None):
@@ -53,13 +61,13 @@ def changedir(path=None):
         print("Not valid directory.")
 
 def ls(path=None):
-    target = path or os.path.dirname(cd)
+    target = path or cd
     if os.path.exists(target) and os.path.isdir(target):
         for name in os.listdir(target): print(name)
     else:
         print("Not a valid directory.")
 
-def newdir(path=None):
+def mkdir(path=None):
     if not path: return print("Not valid.")
     if not os.path.exists(path):
         os.mkdir(path); print(f"'{path}' made successfully.")
@@ -67,15 +75,20 @@ def newdir(path=None):
         print(f"Directory '{path}' already exists.")
 
 def rm(path=None):
-    if not path: return print("Not valid.")
-    if os.path.isdir(path):
-        os.rmdir(path); print(f"Directory '{path}' deleted successfully.")
-    elif os.path.isfile(path):
-        os.remove(path); print(f"File '{path}' deleted successfully.")
-    else:
-        print(f"'{path}' doesn't exist.")
+    if not path:
+        return print("Usage: rm <file or directory>")
 
-def newfile(file=None):
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+        print(f"Directory '{path}' deleted (recursive).")
+    elif os.path.isfile(path):
+        os.remove(path)
+        print(f"File '{path}' deleted.")
+    else:
+        print(f"'{path}' does not exist.")
+
+
+def touch(file=None):
     if not file: return print("Not valid.")
     if not os.path.exists(file):
         contents = input("Enter file content: ")
@@ -84,7 +97,7 @@ def newfile(file=None):
     else:
         print("File already exists.")
 
-def printfile(file=None):
+def cat(file=None):
     if not file: return print("Not valid.")
     if os.path.exists(file):
         with open(file, "r") as f:
@@ -100,7 +113,7 @@ def edit(file=None):
     print(f"File '{file}' updated successfully.")
 
 def ver():
-    print("James Shell v1.0\nPublished on November 4th 2025 with the MIT license by James Baum.")
+    print("James Shell v1.1\nPublished on November 18th 2025 with the MIT license by James Baum.")
 
 def cls(): os.system("cls" if os.name == "nt" else "clear")
 
@@ -141,12 +154,86 @@ def bcolor(arg=None):
     else:
         print("Invalid color. Valid options:", ", ".join(colors.keys()))
 
+def jsh():
+    print("""
+--------  /-----   |    |
+   //     |        |    |
+   /      \-----   |----|
+__/       _____/   |    |
+v1.1 by James Baum
+          """)
+    
+def calc(arg=None):
+    print(eval(arg))
+    
+def blush():
+    print(">_<")
+
+def cookieclicker():
+    global cookie, click
+    cookie = 0
+    click = 1
+
+    def auto_click():
+        global cookie, click
+        while True:
+            time.sleep(1)
+            cookie += click
+
+    threading.Thread(target=auto_click, daemon=True).start()
+
+    print("Type 0 to view stats... type quit to go back to JSH")
+
+    while True:
+        ab = input("> ")
+        if ab == "0":
+            print(f"""
+            {cookie} Cookies
+            {click} Cookies/sec
+            {click} Clickers
+            Type 1 to buy another Clicker
+            """)
+            choice = input("> ")
+            if choice == "1":
+                if cookie < 5:
+                    print("You need at least 5 cookies to buy another clicker!")
+                else:
+                    click += 1
+                    cookie -= 5
+                    print(f"{click} Clickers")
+            elif choice == "quit":
+                break
+        if ab == "quit":
+            break
+
+def jshinterpret(arg=None):
+    if not arg:
+        return print("Please specify a script file.")
+    if not os.path.exists(arg):
+        return print("Script file not found.")
+
+    with open(arg, "r") as f:
+        for line in f:
+            command = line.strip().split(" ", 1)
+            cmd = command[0]
+            arg2 = command[1] if len(command) > 1 else None
+
+            if cmd in commands:
+                try:
+                    commands[cmd](arg2)
+                except TypeError:
+                    commands[cmd]()
+            else:
+                print(f"{cmd} is not a valid command.")
+
 #Command Registry
 commands = {
     "pwd": pwd, "printtext": printtext, "help": help, "cd": changedir,
-    "ls": ls, "newdir": newdir, "rm": rm, "newfile": newfile,
-    "printfile": printfile, "edit": edit, "ver": ver, "clear": cls,
-    "connect": connect, "random": rand, "fcolor": fcolor, "bcolor": bcolor
+    "ls": ls, "mkdir": mkdir, "rm": rm, "touch": touch,
+    "cat": cat, "edit": edit, "ver": ver, "clear": cls,
+    "connect": connect, "random": rand, "fcolor": fcolor, "bcolor": bcolor,
+    "jsh": jsh, "blush": blush, "cookieclicker": cookieclicker, "calc": calc, 
+    "run": jshinterpret
 }
 
 #Main loop
